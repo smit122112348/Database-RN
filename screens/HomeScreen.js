31261ee90ebcaa62d0b507881d24207091b3113a
Smit VaghelaUCC <122112348@umail.ucc.ie>
2023-07-03 12:04:42 +0100

    Adding Updating deleteing and selecting data from realtime db done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,45 +1,114 @@
 import { Button, FlatList, View } from "react-native";
 import { SafeAreaView, Text, TextInput, StyleSheet } from "react-native";
 import { db } from "../src/config";
-import React, { useState } from "react";
-import { ref, set } from "firebase/database";
+import React, { useState, useEffect } from "react";
+import { ref, set, onValue, remove } from "firebase/database";
+
+
+
 
 export default function HomeScreen()
 {
-    const [goals, setGoals] = useState([]);
+    const [editing, setEditing] = useState(false);
+    const [editingGoal, setEditingGoal] = useState(null);
+    const [goalData, setGoalData] = useState([]);
     const [currentGoal, setCurrentGoal] = useState("");
+
+    var TopView = null;
+
+    useEffect(() => {
+        const starCountRef = ref(db, 'posts/');
+        onValue(starCountRef, (snapshot) => {
+            const data = snapshot.val();
+            const newPosts = Object.keys(data).map(key => ({
+                id: key,
+                ...data[key]
+            }));
+            console.log(newPosts);
+            setGoalData(newPosts);
+        });
+
+        if(!editing)
+        {
+            TopView = () => {
+                return(
+                    <View>
+                        <TextInput style={styles.inputs} placeholder="Add you Goal here" onChangeText={setCurrentGoal} value={currentGoal}/>
+                        <Button title="Add Goal" onPress={addButtonHandler}/>
+                    </View>
+                );
+            }
+        }
+        else
+        {
+            TopView = () => {
+                return(
+                    <View>
+                    <TextInput style={styles.inputs} placeholder="" onChangeText={setCurrentGoal} value={setCurrentGoal}/>
+                    <Button title="Edit Goal" onPress={updateButtonHandler}/>
+                </View>
+                );
+            }
+        }
+
+
+    }, []);
 
     function addButtonHandler()
     {
-        setGoals((currentGoals) => [...currentGoals, {
-            text: currentGoal,
-            id: Math.random().toString()
-        }]);
-
-        var newGoal = {
-            name: currentGoal.text
-        }
-
         set(ref(db, 'posts/' + currentGoal), {
             goal: currentGoal
-        });
-        setCurrentGoal("")
+        }).then();
+        setCurrentGoal("");
+        setEditing(false);
+    }
+
+    function updateButtonHandler()
+    {
+        set(ref(db, 'posts/' + editingGoal.id), {
+            goal: currentGoal
+        }).then();
+        setCurrentGoal("");
+        setEditing(false);
+    }
+
+    function editButtonHandler(id)
+    {
+        const searchObject= goalData.find((goal) => goal.id === id);
+        setCurrentGoal(searchObject.goal);
+        setEditingGoal(searchObject);
+        setEditing(true);
+    }
+
+    function deleteButtonHandler(id)
+    {
+        remove(ref(db, 'posts/' + id)).then(() => {console.log("deleted")}).catch((error) => {console.log("error: ", error)});
     }
 
     return (
         <SafeAreaView>
+            {editing ? (
             <View>
+                <TextInput style={styles.inputs} placeholder="" onChangeText={setCurrentGoal} value={currentGoal}/>
+                <Button title="Edit Goal" onPress={updateButtonHandler}/>
+                <Button title="Add Goal" onPress={addButtonHandler}/>
+            </View>
+        ) : <View>
                 <TextInput style={styles.inputs} placeholder="Add you Goal here" onChangeText={setCurrentGoal} value={currentGoal}/>
                 <Button title="Add Goal" onPress={addButtonHandler}/>
             </View>
+            }
             <View>
                 <FlatList 
                     keyExtractor={(item, index) => {return item.id}}
-                    data={goals}
+                    data={goalData}
                     renderItem={(itemData) => {
                         return (
                         <View>
-                            <Text>{itemData.item.text}</Text>
+                            <Text>goal: {itemData.item.goal}</Text>
+                            <Text>id: {itemData.item.id}</Text>
+                            <Button title="Edit" onPress={() => {editButtonHandler(itemData.item.id)}} />
+                            <Button title="Delete" onPress={() => {deleteButtonHandler(itemData.item.id)}} />
                         </View>);
                     }}
                 />
